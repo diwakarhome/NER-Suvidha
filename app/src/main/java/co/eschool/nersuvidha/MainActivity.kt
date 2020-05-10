919b16b7d4bfdaafe69cf7fb206a09e2b216edbd
diwakarsinghdiwakar <diwakersingh31@gmail.com>
2020-05-10 18:58:19 +0530

    updated dependencies

--- app/src/main/java/co/eschool/nersuvidha/MainActivity.kt
+++ app/src/main/java/co/eschool/nersuvidha/MainActivity.kt
@@ -1,7 +1,7 @@
 package co.eschool.nersuvidha
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
